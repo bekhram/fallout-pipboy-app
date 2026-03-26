dc7db0c2bf7e9da6eb0d098c2346672483d8d093
bekhram <bekhram.salimov@airslate.com>
2026-03-26 15:21:45 +0100

    fix PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,47 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "./",
+  base: "/",
   server: {
     host: true,
     port: 5173,
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Pip-2d20",
+        short_name: "Pip2d20",
+        description: "F2d20 app",
+        theme_color: "#0b3d2e",
+        background_color: "#071b14",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
