393880e97c25451c8cbf917fc39858e62e24da48
bekhram <bekhram.salimov@airslate.com>
2026-03-26 16:55:49 +0100

    fix pipboy css import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./pipboy.css";
+import "./styles/pipboy.css";
 import "./i18n";
 import "./styles/dice.css";
 
