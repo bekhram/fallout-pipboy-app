f4c0ebb2b06fddb480e503c143a7cf23bbc9425f
Bekhram Salimov <bekhramsalimov@Bekhrams-MacBook-Pro.local>
2026-09-04 13:49:44 +0200

    feat: continue Auto GM improvements

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3udahk2af6c"
+    "revision": "0.9l7enb9n3ug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
