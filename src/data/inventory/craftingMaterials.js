9a9a523ab473dc96d6fd344f287b8f8e61d1ca2b
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-05 12:44:26 +0200

    Move crafting materials to junk category

--- src/data/inventory/craftingMaterials.js
+++ src/data/inventory/craftingMaterials.js
@@ -1,7 +1,7 @@
 export const CRAFTING_MATERIAL_ITEMS = [
-  { name: "Common Materials", category: "misc", sourceType: "crafting_material", materialTier: "common", weight: "1", cost: "1", effect: "Common crafting material unit." },
-  { name: "Uncommon Materials", category: "misc", sourceType: "crafting_material", materialTier: "uncommon", weight: "1", cost: "3", effect: "Uncommon crafting material unit." },
-  { name: "Rare Materials", category: "misc", sourceType: "crafting_material", materialTier: "rare", weight: "1", cost: "5", effect: "Rare crafting material unit." },
+  { name: "Common Materials", category: "junk", sourceType: "crafting_material", materialTier: "common", weight: "1", cost: "1", effect: "Common crafting material unit." },
+  { name: "Uncommon Materials", category: "junk", sourceType: "crafting_material", materialTier: "uncommon", weight: "1", cost: "3", effect: "Uncommon crafting material unit." },
+  { name: "Rare Materials", category: "junk", sourceType: "crafting_material", materialTier: "rare", weight: "1", cost: "5", effect: "Rare crafting material unit." },
 ];
 
 const LOCALIZED = {
