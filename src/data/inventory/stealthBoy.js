40f14e71ba23a3866f2f5779d8edfd494cde60cf
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-05 13:02:10 +0200

    Add standalone Stealth Boy inventory item

---
+++ src/data/inventory/stealthBoy.js
@@ -0,0 +1,22 @@
+export const STEALTH_BOY_ITEM = {
+  name: "Stealth Boy",
+  category: "misc",
+  effect: "Invisibility",
+  weight: "1",
+  cost: "100",
+  rarity: "5",
+  localizedName: {
+    en: "Stealth Boy",
+    ru: "Стелс-бой",
+    uk: "Стелс-бой",
+    pl: "Stealth Boy",
+  },
+  localizedEffect: {
+    en: "Invisibility",
+    ru: "Невидимость",
+    uk: "Невидимість",
+    pl: "Niewidzialność",
+  },
+};
+
+export default STEALTH_BOY_ITEM;
