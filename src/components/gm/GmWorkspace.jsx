fb91a3cba26fd62215de716756246a9dcdb1a671
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-06 21:50:24 +0200

    feat: render live session map in GM workspace

--- src/components/gm/GmWorkspace.jsx
+++ src/components/gm/GmWorkspace.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import GmPanel from "./GmPanel.jsx";
 import GmToolkit from "./GmToolkit.jsx";
+import GmSessionMap from "./GmSessionMap.jsx";
 import "./gmWorkspace.css";
+import "./gmSessionMapLayout.css";
 
 export default function GmWorkspace({ character = null, setCharacter = null, onOpenMap }) {
   const [panelVersion, setPanelVersion] = useState(0);
 
   return (
     <div className="gm-workspace">
+      <GmSessionMap character={character} />
       <GmPanel
         key={`gm-panel-${panelVersion}`}
         character={character}
