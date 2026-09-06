1924ac49d7c2cbe46ae6edfbbaa58f54c96af1d8
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-06 19:37:08 +0200

    feat: add GM workspace toolkit wrapper

---
+++ src/components/gm/GmWorkspace.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import GmPanel from "./GmPanel.jsx";
+import GmToolkit from "./GmToolkit.jsx";
+
+export default function GmWorkspace({ character = null, setCharacter = null, onOpenMap }) {
+  const [panelVersion, setPanelVersion] = useState(0);
+
+  return (
+    <div className="gm-workspace">
+      <GmPanel
+        key={`gm-panel-${panelVersion}`}
+        character={character}
+        onOpenMap={onOpenMap}
+      />
+      <GmToolkit
+        character={character}
+        setCharacter={setCharacter}
+        onGmStateChanged={() => setPanelVersion((value) => value + 1)}
+      />
+    </div>
+  );
+}
