b8ebec4bc66c520d8e4db52498428e42dd51a4c1
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-07 22:59:00 +0200

    feat: use GM-assigned token session layer

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV3, {
+import useGmAuthoritativeSessionV4, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV3.js";
+} from "./useGmAuthoritativeSessionV4.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV3(form);
+  const session = useGmAuthoritativeSessionV4(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
