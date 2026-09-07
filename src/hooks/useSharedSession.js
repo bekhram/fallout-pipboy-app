ef0756ccc40932f44e59dc159fd3fd790b5593e8
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 00:58:22 +0200

    feat: enable tactical session v5 layer

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV4, {
+import useGmAuthoritativeSessionV5, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV4.js";
+} from "./useGmAuthoritativeSessionV5.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV4(form);
+  const session = useGmAuthoritativeSessionV5(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
