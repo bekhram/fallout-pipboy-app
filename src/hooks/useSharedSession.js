29ec1622133904c62e23640927c860c92a0da8aa
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-07 21:26:52 +0200

    refactor: use multi-scene cached session v3

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSession, {
+import useGmAuthoritativeSessionV2, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSession.js";
+} from "./useGmAuthoritativeSessionV2.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSession(form);
+  const session = useGmAuthoritativeSessionV2(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
