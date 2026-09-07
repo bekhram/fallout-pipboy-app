3be2666830004c90312ae3acfa683f3e51ea470a
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 01:34:54 +0200

    feat: enable live player stat sync

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV5, {
+import useGmAuthoritativeSessionV6, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV5.js";
+} from "./useGmAuthoritativeSessionV6.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV5(form);
+  const session = useGmAuthoritativeSessionV6(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
