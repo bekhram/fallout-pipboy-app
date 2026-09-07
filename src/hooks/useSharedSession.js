ad161e138f0be2b081d84ddc55655db9e62230cc
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-07 20:14:40 +0200

    refactor: expose Socket.IO session to GM tactical tools

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,6 +1,20 @@
-export {
-  default,
+import { useEffect } from "react";
+import useSocketSession, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
 } from "./useSocketSession.js";
+import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
+
+export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
+
+export default function useSharedSession(form) {
+  const session = useSocketSession(form);
+
+  useEffect(() => {
+    setLiveSessionBridge(session);
+    return () => setLiveSessionBridge(null);
+  }, [session]);
+
+  return session;
+}
