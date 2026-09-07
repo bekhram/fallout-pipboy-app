b6bcf131d922996b8520ee861abf53b7319db94b
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-07 22:33:22 +0200

    fix: use non-blocking player token asset session

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV2, {
+import useGmAuthoritativeSessionV3, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV2.js";
+} from "./useGmAuthoritativeSessionV3.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV2(form);
+  const session = useGmAuthoritativeSessionV3(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
