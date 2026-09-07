1a44745bf3fa3077476e0fb0e8ec5874b870a211
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-07 21:14:07 +0200

    refactor: use GM-authoritative cached session transport

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useSocketSession, {
+import useGmAuthoritativeSession, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useSocketSession.js";
+} from "./useGmAuthoritativeSession.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useSocketSession(form);
+  const session = useGmAuthoritativeSession(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
