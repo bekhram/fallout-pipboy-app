73c08d2a88a492c63a9be74d81ca6df1e58fda91
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 23:02:12 +0200

    Use stable footprint session wrapper

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV11, {
+import useGmAuthoritativeSessionV12, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV11.js";
+} from "./useGmAuthoritativeSessionV12.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV11(form);
+  const session = useGmAuthoritativeSessionV12(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
