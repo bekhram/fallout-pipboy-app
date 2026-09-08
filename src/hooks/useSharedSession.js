c3e72df99c56cfd17576023c361c65a6120c88c6
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 22:06:02 +0200

    Use 3x3 token-safe session wrapper

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV10, {
+import useGmAuthoritativeSessionV11, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV10.js";
+} from "./useGmAuthoritativeSessionV11.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV10(form);
+  const session = useGmAuthoritativeSessionV11(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
