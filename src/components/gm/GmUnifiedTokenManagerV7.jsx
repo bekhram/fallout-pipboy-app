8e7367e0eadb20d701a2c3453e57b34144805262
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 21:50:49 +0200

    Switch GM NPC creation to character-style combat editor

--- src/components/gm/GmUnifiedTokenManagerV7.jsx
+++ src/components/gm/GmUnifiedTokenManagerV7.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import GmUnifiedTokenManagerV5 from "./GmUnifiedTokenManagerV5.jsx";
-import GmNpcCardEditorV7 from "./GmNpcCardEditorV7.jsx";
+import GmNpcCardEditorV8 from "./GmNpcCardEditorV8.jsx";
 
 export default function GmUnifiedTokenManagerV7({ session }) {
   const tokenSession = useMemo(() => {
@@ -14,7 +14,7 @@ export default function GmUnifiedTokenManagerV7({ session }) {
   return (
     <div className="gm-unified-token-manager-v7">
       <GmUnifiedTokenManagerV5 session={tokenSession} />
-      <GmNpcCardEditorV7 session={session} />
+      <GmNpcCardEditorV8 session={session} />
     </div>
   );
 }
