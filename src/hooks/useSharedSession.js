031be0a5045f6488a983af293b4e85dc4cf11fa3
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 06:29:09 +0200

    feat: enable player card sync for gm auto gm

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV8, {
+import useGmAuthoritativeSessionV9, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV8.js";
+} from "./useGmAuthoritativeSessionV9.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV8(form);
+  const session = useGmAuthoritativeSessionV9(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
