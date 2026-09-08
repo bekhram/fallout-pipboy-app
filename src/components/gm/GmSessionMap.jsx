630b6d3ec82f8253c19342368c529b183d9cb033
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-08 23:49:11 +0200

    Add random encounter map presets to GM scenes

--- src/components/gm/GmSessionMap.jsx
+++ src/components/gm/GmSessionMap.jsx
@@ -7,6 +7,7 @@ import BattlemapViewportControls from "./BattlemapViewportControls.jsx";
 import GmAutoGmPanel from "./GmAutoGmPanel.jsx";
 import GmLootGenerator from "./GmLootGenerator.jsx";
 import GmMerchantGenerator from "./GmMerchantGenerator.jsx";
+import GmScenePresetPanel from "./GmScenePresetPanel.jsx";
 import TacticalEnvironmentPanel, {
   TacticalEnvironmentSummary,
 } from "./TacticalEnvironmentPanel.jsx";
@@ -82,6 +83,7 @@ export default function GmSessionMap(props) {
         <div className="gm-tactical-loot"><GmLootGenerator session={session} /></div>
         <div className="gm-tactical-merchants"><GmMerchantGenerator session={session} /></div>
         <div className="gm-tactical-environment-edit"><TacticalEnvironmentPanel scene={session.tacticalScene} session={session} /></div>
+        <div className="gm-tactical-scene-presets"><GmScenePresetPanel session={session} /></div>
         <div className="gm-tactical-map-core"><GmSessionMapV2 {...props} session={session} /></div>
         <BattlemapViewportControls session={session} role="gm" activeTab={activeTab} />
         <GmTokenStatusLayer session={session} />
