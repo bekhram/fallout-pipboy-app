8f8f84d2f116e2dcfb99984e2740dca9651ce05b
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 12:11:36 +0200

    feat: activate compact procedural session V14

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV13, {
+import useGmAuthoritativeSessionV14, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV13.js";
+} from "./useGmAuthoritativeSessionV14.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV13(form);
+  const session = useGmAuthoritativeSessionV14(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
