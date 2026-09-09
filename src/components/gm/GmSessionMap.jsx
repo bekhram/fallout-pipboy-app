eddb34e400863a1bb26eb4e439c2cf0c6bebb405
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 20:59:38 +0200

    feat: use procedural difficulty scene panel

--- src/components/gm/GmSessionMap.jsx
+++ src/components/gm/GmSessionMap.jsx
@@ -7,8 +7,8 @@ import BattlemapViewportControls from "./BattlemapViewportControls.jsx";
 import GmAutoGmPanel from "./GmAutoGmPanel.jsx";
 import GmLootGenerator from "./GmLootGenerator.jsx";
 import GmMerchantGenerator from "./GmMerchantGenerator.jsx";
-import GmScenePresetPanel from "./GmScenePresetPanel.jsx";
-import GmProceduralRoomDescriptions from "./GmProceduralRoomDescriptions.jsx";
+import GmScenePresetPanelV2 from "./GmScenePresetPanelV2.jsx";
+import GmProceduralRoomDescriptionsV2 from "./GmProceduralRoomDescriptionsV2.jsx";
 import TacticalEnvironmentPanel, {
   TacticalEnvironmentSummary,
 } from "./TacticalEnvironmentPanel.jsx";
@@ -85,8 +85,8 @@ export default function GmSessionMap(props) {
         <div className="gm-tactical-merchants"><GmMerchantGenerator session={session} /></div>
         <div className="gm-tactical-environment-edit"><TacticalEnvironmentPanel scene={session.tacticalScene} session={session} /></div>
         <div className="gm-tactical-scene-presets">
-          <GmScenePresetPanel session={session} />
-          <GmProceduralRoomDescriptions session={session} />
+          <GmScenePresetPanelV2 session={session} />
+          <GmProceduralRoomDescriptionsV2 session={session} />
         </div>
         <div className="gm-tactical-map-core"><GmSessionMapV2 {...props} session={session} /></div>
         <BattlemapViewportControls session={session} role="gm" activeTab={activeTab} />
