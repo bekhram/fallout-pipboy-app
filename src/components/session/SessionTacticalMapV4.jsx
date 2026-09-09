796a50be05941e1c5dd799a9cd563b82b7682955
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 17:43:58 +0200

    refactor: remove player procedural interaction overlay

--- src/components/session/SessionTacticalMapV4.jsx
+++ src/components/session/SessionTacticalMapV4.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import SessionTacticalMapV3 from "./SessionTacticalMapV3.jsx";
-import PlayerProceduralMapSemanticPortal from "./PlayerProceduralMapSemanticPortal.jsx";
-import PlayerTacticalCoverBadge from "./PlayerTacticalCoverBadge.jsx";
 import "../gm/tacticalFootprint3.css";
 
 export default function SessionTacticalMapV4(props) {
-  return (
-    <>
-      <SessionTacticalMapV3 {...props} />
-      <PlayerProceduralMapSemanticPortal scene={props.session?.tacticalScene} />
-      <PlayerTacticalCoverBadge session={props.session} />
-    </>
-  );
+  return <SessionTacticalMapV3 {...props} />;
 }
