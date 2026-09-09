68323e39bb00c0a9752d72f3962500eb28aa3eb9
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 12:04:41 +0200

    feat: enable procedural collision session V13

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV12, {
+import useGmAuthoritativeSessionV13, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV12.js";
+} from "./useGmAuthoritativeSessionV13.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV12(form);
+  const session = useGmAuthoritativeSessionV13(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
