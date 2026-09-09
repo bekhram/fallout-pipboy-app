4f6f402608265af89468f8afe8ed8c891923b042
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 12:17:31 +0200

    feat: display player cover state on tactical map

--- src/components/session/SessionTacticalMapV4.jsx
+++ src/components/session/SessionTacticalMapV4.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SessionTacticalMapV3 from "./SessionTacticalMapV3.jsx";
 import PlayerProceduralMapSemanticPortal from "./PlayerProceduralMapSemanticPortal.jsx";
+import PlayerTacticalCoverBadge from "./PlayerTacticalCoverBadge.jsx";
 import "../gm/tacticalFootprint3.css";
 
 export default function SessionTacticalMapV4(props) {
@@ -8,6 +9,7 @@ export default function SessionTacticalMapV4(props) {
     <>
       <SessionTacticalMapV3 {...props} />
       <PlayerProceduralMapSemanticPortal scene={props.session?.tacticalScene} />
+      <PlayerTacticalCoverBadge session={props.session} />
     </>
   );
 }
