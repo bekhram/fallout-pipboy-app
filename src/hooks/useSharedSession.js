c58cb688c5acd26df9a459cce2b076ed4aba8f2a
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-09 17:43:18 +0200

    feat: activate visual-only procedural session V15

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useGmAuthoritativeSessionV14, {
+import useGmAuthoritativeSessionV15, {
   GAME_SERVER_URL,
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
-} from "./useGmAuthoritativeSessionV14.js";
+} from "./useGmAuthoritativeSessionV15.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
-  const session = useGmAuthoritativeSessionV14(form);
+  const session = useGmAuthoritativeSessionV15(form);
 
   useEffect(() => {
     setLiveSessionBridge(session);
