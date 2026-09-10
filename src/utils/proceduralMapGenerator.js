2b3395280217a25c9264f21c74f1772135f9b5fa
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-10 13:31:23 +0200

    feat: enable residential house map generator

--- src/utils/proceduralMapGenerator.js
+++ src/utils/proceduralMapGenerator.js
@@ -1,4 +1,4 @@
-import * as V6 from "./proceduralMapGeneratorV6.js";
+import * as V7 from "./proceduralMapGeneratorV7.js";
 import { normalizeEnemyGroup } from "./proceduralEnemyGroups.js";
 
 const RARITIES = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7"];
@@ -22,24 +22,24 @@ function legacyInput(value = {}) {
   return { ...value, lootRarity: R_TO_LEGACY[rarity] };
 }
 
-export const MAP_TYPES = V6.MAP_TYPES;
-export const makeProceduralSeed = V6.makeProceduralSeed;
-export const proceduralLocationType = V6.proceduralLocationType;
+export const MAP_TYPES = V7.MAP_TYPES;
+export const makeProceduralSeed = V7.makeProceduralSeed;
+export const proceduralLocationType = V7.proceduralLocationType;
 
 export function normalizeProceduralMapSpec(value = {}) {
   const rarity = normalizeRarity(value.lootRarity);
-  const base = V6.normalizeProceduralMapSpec(legacyInput(value));
+  const base = V7.normalizeProceduralMapSpec(legacyInput(value));
   return {
     ...base,
     lootRarity: rarity,
     encounterDifficulty: normalizeDifficulty(value.encounterDifficulty ?? value.difficulty),
     enemyFaction: normalizeEnemyGroup(value.enemyFaction ?? value.enemyGroup ?? "auto"),
-    version: Math.max(10, Number(base.version || 0)),
+    version: Math.max(11, Number(base.version || 0)),
   };
 }
 
 export function generateProceduralMapSvg(input = {}) {
-  return V6.generateProceduralMapSvg(legacyInput(input));
+  return V7.generateProceduralMapSvg(legacyInput(input));
 }
 
 export function generateProceduralMapDataUrl(input = {}) {
