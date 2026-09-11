68f33bba5b07bac937d5409d36d3ad6c9ec9c77d
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-12 00:51:30 +0200

    feat: show shared battlemap drawings and pings to players

--- src/components/session/SessionTacticalMapV4.jsx
+++ src/components/session/SessionTacticalMapV4.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import SessionTacticalMapV3 from "./SessionTacticalMapV3.jsx";
+import BattlemapSharedLayer from "../gm/BattlemapSharedLayer.jsx";
 import wastelandBg from "../../assets/wasteland/backgrounds/wasteland-bg-1.png";
 import swampBg from "../../assets/wasteland/backgrounds/wasteland-swamp-bg-1.png";
 import urbanRuinsBg from "../../assets/wasteland/backgrounds/wasteland-urban-ruins-bg-1.png";
@@ -89,5 +90,8 @@ export default function SessionTacticalMapV4(props) {
     return () => window.clearTimeout(timer);
   }, [scene?.active, scene?.sceneId, scene?.backgroundUrl, isWasteland, terrain]);
 
-  return <SessionTacticalMapV3 {...props} />;
+  return <>
+    <SessionTacticalMapV3 {...props} />
+    <BattlemapSharedLayer scene={scene} role="player" />
+  </>;
 }
