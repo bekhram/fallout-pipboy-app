70543f529549f9011e5c45d76fbe0ff613d2808d
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-16 03:38:59 +0200

    feat: sync shared sessions with Firestore campaigns

--- src/hooks/useSharedSession.js
+++ src/hooks/useSharedSession.js
@@ -4,18 +4,21 @@ import useGmAuthoritativeSessionV15, {
   SESSION_CODE_LENGTH,
   normalizeSessionCode,
 } from "./useGmAuthoritativeSessionV15.js";
+import useCloudCampaignSync from "./useCloudCampaignSync.js";
 import { setLiveSessionBridge } from "../utils/liveSessionBridge.js";
 
 export { GAME_SERVER_URL, SESSION_CODE_LENGTH, normalizeSessionCode };
 
 export default function useSharedSession(form) {
   const session = useGmAuthoritativeSessionV15(form);
+  const cloud = useCloudCampaignSync(session, form);
+  const mergedSession = { ...session, ...cloud };
 
   useEffect(() => {
-    setLiveSessionBridge(session);
-  }, [session]);
+    setLiveSessionBridge(mergedSession);
+  }, [mergedSession]);
 
   useEffect(() => () => setLiveSessionBridge(null), []);
 
-  return session;
+  return mergedSession;
 }
