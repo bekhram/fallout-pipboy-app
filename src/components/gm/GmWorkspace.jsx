17d1527fbca6db6123ad067daecebdee04ea48cf
Bekhram Salimov <bahramsalimov89@gmail.com>
2026-09-16 19:01:21 +0200

    refactor: remove manual quest selector

--- src/components/gm/GmWorkspace.jsx
+++ src/components/gm/GmWorkspace.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import GmSessionMap from "./GmSessionMap.jsx";
-import QuestTypeSelectorPortal from "./QuestTypeSelectorPortal.jsx";
 import { useLiveSessionBridge } from "../../utils/liveSessionBridge.js";
 import "./gmWorkspace.css";
 import "./gmSessionMapLayout.css";
@@ -14,7 +13,6 @@ export default function GmWorkspace({ character = null, session = null }) {
     <div className="gm-workspace gm-workspace--tactical-only">
       <div ref={tacticalMapRef} className="gm-workspace__tactical-anchor">
         <GmSessionMap character={character} session={effectiveSession} />
-        <QuestTypeSelectorPortal session={effectiveSession} />
       </div>
     </div>
   );
